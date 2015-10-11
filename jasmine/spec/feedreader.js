46d74f0fd7afe9382b6a1d937f7ed5965e36c5d0
Wisnu Mulya <wisnu@wisnumulya.com>
2015-10-11 21:36:19 +0700

    test: Add test to ensure each object in allFeeds has a name

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -38,10 +38,15 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+        /* This test makes sure that each object in allFeeds has
+         * a name defined and that the name is not empty.
          */
+        it('should have a defined name', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            })
+        });
     });
 
 
